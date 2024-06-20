ee309d721fcc513e2e6cffb380f91c8daf4417dc
Neovni <lethanaxeger@gmail.com>
2024-06-20 17:41:17 +0700

    Added some component

--- app/src/main/java/com/example/akhir/MainActivity.kt
+++ app/src/main/java/com/example/akhir/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.akhir
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_jajan.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewPagerAdapter: ViewPageAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPagerAdapter = ViewPageAdapter(supportFragmentManager, lifecycle)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when(position){
+                0 -> tab.text = "Jajan"
+                1 -> tab.text = "Makanan Berat"
+            }
+        }.attach()
     }
+
 }
---
+++ app/src/main/java/com/example/akhir/MakananFragment.kt
@@ -0,0 +1,7 @@
+package com.example.akhir
+
+import androidx.fragment.app.Fragment
+
+class MakananFragment : Fragment() {
+
+}
